44d3dfb0ea55782ebc06bbab488bb8863ed873ae
EmptyBottle <ttes3511@hotmail.com>
2022-06-21 22:06:40 +0900

    SDCreate関数で実態を返すよう変更

--- SDUtil.c
+++ SDUtil.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "SDUtil.h"
 
-SettingData* SDCreate(char* key, enum SDDataType type, void* value)
+SettingData SDCreate(char* key, enum SDDataType type, void* value)
 {
     SettingData data;
     data.key = key;
     data.type = type;
     data.value = value;
-    return &data;
+    return data;
 }
