1933df65e1611eaa4dda093b545319ce1640b20a
EmptyBottle <ttes3511@hotmail.com>
2022-06-21 21:37:24 +0900

    LoadSettingsを編集

--- SettingsManager.c
+++ SettingsManager.c
@@ -7,10 +7,23 @@ SettingsData settings;
 void LoadSettings()
 {
     FILE* fp;
-    errno_t err = fopen_s(fp, "settings.txt", "r");
-    if(err != 0)
+    errno_t er1, er2;
+    er1 = fopen_s(fp, "settings.txt", "r");
+    if(er1 != 0)
     {// ファイルが存在しないか開けない場合
+        er2 = fopen_s(fp, "settings.txt", "w");
+        if(er2 != 0)
+        {// ファイルが作れない場合
+            printf("エラー: 設定ファイルが読み込めませんでした。\n初期設定のまま続行します。\n");
+            return;
+        }
+        else
+        {// ファイルが作れた場合
+            // セーブ処理
+        }
     }
+    //読み込み処理
+    
 }
 
 void initSettings()
