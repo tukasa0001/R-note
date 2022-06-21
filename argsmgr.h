0e093cbbb3892e74bf0e900f39aaac8fa2081614
EmptyBottle <ttes3511@hotmail.com>
2022-06-21 18:00:24 +0900

    スペルミスと";"の記入漏れを修正

--- argsmgr.h
+++ argsmgr.h
@@ -1,13 +1,13 @@
-#ifdef ARGSMGR_H
+#ifndef ARGSMGR_H
 #define ARGSMGR_H
 enum Operation
 {
     Create = 0
-}
+};
 
 struct argument
 {
     enum Operation operation;
     char* tag;
-}
+};
 #endif
