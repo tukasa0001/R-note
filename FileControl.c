74a0b75bfad1f35f1f368677b054d7e99a687740
EmptyBottle <ttes3511@hotmail.com>
2022-06-21 19:30:04 +0900

    FileControlに関数を追加

--- FileControl.c
+++ FileControl.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
 #include "FileControl.h";
+
+void CreateFile(char* tag)
+{
+    char* filename = getFileName(tag);
+    FILE* file;
+    file = fopen(filename, "w+");
+    fprintf(file, "ファイル作成テスト(%s)", filename);
+    fclose(file);
+}
+
+char* getFileName(char* tag)
+{
+    char* filename[32];
+    time_t now = time(NULL);
+    // 2000.01.01-00.00.00
+    strftime(filename, sizeof(filename), "%Y.%m.%d-%H.%M.%S", localtime(&now));
+    strcat(filename, ".txt");
+    return filename;
+}
