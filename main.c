f0a8bc09f7490028530db49f294e3c2107866c10
EmptyBottle <ttes3511@hotmail.com>
2022-06-22 02:26:29 +0900

    LoadSettingsをmain関数内で実行するよう追加

--- main.c
+++ main.c
@@ -12,6 +12,8 @@ void main(int argc, char *argv[])
     initArgs(&args, argc, argv);
     initSettings();
 
+    LoadSettings(&settings);
+
     printf("argument構造体の状態: \n");
     printf("    operationID: %d\n", (int)args.operation);
     printf("    tag: %s\n", args.tag);
