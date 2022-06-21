158e25499608ce98200839b921ecbeaac8f4ab1a
EmptyBottle <ttes3511@hotmail.com>
2022-06-21 22:57:13 +0900

    SettingData配列から特定のキーの項目を見つける関数を追加

--- SDUtil.c
+++ SDUtil.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "SDUtil.h"
 
 void SDInit(SettingData* data, char* key, enum SDDataType type, void* value)
@@ -18,4 +19,12 @@ void SDInitInt(SettingData* data, char* key, int* num)
     data->key = key;
     data->type = Int;
     data->value = num;
+}
+SettingData* SDFindIndex(SettingData* data[], size_t size, char* key)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        if(strcmp(data[i]->key, key)) return &data[i];
+    }
+    return NULL;
 }
