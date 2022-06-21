89b369fbf4ea9c5d6854041043bad0ebff4b9262
EmptyBottle <ttes3511@hotmail.com>
2022-06-21 18:25:13 +0900

    initArgsの中身を作成

--- argsmgr.c
+++ argsmgr.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "argsmgr.h"
 
 argument* initArgs(int argc, char* argv[])
 {
+    argument args;
+    int i = 0;
+    bool isTagAssigned = false;
+
+    args.operation = Create;
+    for (int i = 0; i < argc; i++)
+    {
+        char* arg = argv[i];
+        switch(*arg)
+        {
+            case '-':
+                // プロパティを表す予定だが未実装
+                printf("警告: プロパティは未実装です");
+                break;
+            default:
+                if(!isTagAssigned)
+                {// タグとして割りあてる
+                    args.tag = arg;
+                }
+                else
+                {
+                    printf("エラー: 不正な引数: %s\n何かキーを押して終了します", arg);
+                    getchar();
+                    exit();
+                }
+                break;
+        }
+    }
+
+    return &args;
 }
