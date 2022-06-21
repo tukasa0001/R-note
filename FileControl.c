31e0af25c4f529d782b312bd681315bdb8500254
EmptyBottle <ttes3511@hotmail.com>
2022-06-21 20:30:58 +0900

    タグを反映させるよう変更

--- FileControl.c
+++ FileControl.c
@@ -8,7 +8,7 @@
 //char* tag: コマンドライン引数で指定されたタグ
 void CreateFile(char* tag)
 {
-    char filename[30];
+    char filename[84];
     getFileName(filename, sizeof(filename), tag);
     FILE* fp;
     errno_t en1;
@@ -31,8 +31,15 @@ void getFileName(char* filename, long long unsigned int size, char* tag)
         return;
     }
     time_t now = time(NULL);
+    char timestamp[20];
     // 2000.01.01-00.00.00
-    strftime(filename, size, "%Y.%m.%d-%H.%M.%S", localtime(&now));
-    strcat(filename, ".txt");
+    strftime(timestamp, sizeof(timestamp), "%Y.%m.%d-%H.%M.%S", localtime(&now));
+
+    if(tag != NULL && strlen(timestamp)+strlen(tag)+strlen("txt")+4 <= size)
+    {
+        snprintf(filename, size, "%s-[%s].%s", timestamp, tag, "txt");
+    }
+    else snprintf(filename, size, "%s.%s", timestamp, "txt");
+    
     printf("filename: %s", filename);
 }
