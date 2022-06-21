b7dcb04eeb9af207de4d73708ff67f86d20d139b
EmptyBottle <ttes3511@hotmail.com>
2022-06-21 20:44:25 +0900

    SettingsData構造体を定義 関数を定義

--- SettingsManager.c
+++ SettingsManager.c
@@ -1,2 +1,7 @@
 #include <stdio.h>
 #include "SettingsManager.h"
+
+void LoadSettings()
+{
+    
+}
--- SettingsManager.h
+++ SettingsManager.h
@@ -1,4 +1,13 @@
 #ifndef SETTINGSMGR_H
 #define SETTINGSMGR_H
+//include
+#include <stdio.h>
+//構造体
+typedef struct
+{
+    char* path[160];
+    char* extension[10];
+} SettingsData;
 
+void LoadSettings();
 #endif
