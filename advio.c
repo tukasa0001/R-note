a87dc5b34091639e650d4557d8ebcd413fe8cdb0
EmptyBottle <ttes3511@hotmail.com>
2022-06-25 03:04:38 +0900

    scanYesOrNo関数を追加

--- advio.c
+++ advio.c
@@ -1,2 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 #include "advio.h"
 
+bool scanYesOrNo()
+{
+    char str[10];
+    while(true)
+    {
+        scanf_s("%s", str, 10);
+        //YES判定
+        if(strcmp(str, "Yes") +
+            strcmp(str, "yes") +
+            strcmp(str, "YES") +
+            strcmp(str, "Y") +
+            strcmp(str, "y") +
+            strcmp(str, "True") +
+            strcmp(str, "true") +
+            strcmp(str, "TRUE") +
+            strcmp(str, "T") +
+            strcmp(str, "t")
+            == 1
+        ) return true;
+        //NO判定
+        if(strcmp(str, "No") +
+            strcmp(str, "no") +
+            strcmp(str, "NO") +
+            strcmp(str, "N") +
+            strcmp(str, "n") +
+            strcmp(str, "False") +
+            strcmp(str, "false") +
+            strcmp(str, "FALSE") +
+            strcmp(str, "F") +
+            strcmp(str, "f")
+            == 1
+        ) return false;
+    }
+}
--- advio.h
+++ advio.h
@@ -1,4 +1,6 @@
 #ifndef ADVIO_H
 #define ADVIO_h
 
+//プロトタイプ宣言
+bool scanYesOrNo();
 #endif
